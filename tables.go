fb98858f7f094d75ea0505f1705d1b813fe26e2c
Hank Shen <swhbox@foxmail.com>
2021-06-21 11:07:53 +0800

    update

--- tables.go
+++ tables.go
@@ -13,7 +13,7 @@ var (
 			"`group` varchar(32) NOT NULL COMMENT '任务集群',\n" +
 			"`cron` varchar(32) NOT NULL DEFAULT '' COMMENT 'cron表达式',\n" +
 			"`target` varchar(255) NOT NULL COMMENT '目标任务',\n" +
-			"`params` varchar(255) NOT NULL DEFAULT '' COMMENT '参数',\n" +
+			"`params` varchar(2000) NOT NULL DEFAULT '' COMMENT '参数',\n" +
 			"`ip` varchar(32) NOT NULL DEFAULT '' COMMENT 'ip',\n" +
 			"`status` tinyint(4) NOT NULL DEFAULT '3' COMMENT '状态(1-执行中;2-完成;3-未知;4-错误)',\n" +
 			"`mobile` varchar(32) NOT NULL DEFAULT '' COMMENT '手机号码',\n" +
@@ -22,7 +22,7 @@ var (
 			"`start_time` varchar(32) NOT NULL DEFAULT '' COMMENT '开始时间',\n" +
 			"`finish_time` varchar(32) NOT NULL DEFAULT '' COMMENT '结束时间',\n" +
 			"`times` bigint(20) NOT NULL DEFAULT '0' COMMENT '耗时',\n" +
-			"`result` varchar(255) NOT NULL DEFAULT '' COMMENT '返回结果',\n" +
+			"`result` varchar(2000) NOT NULL DEFAULT '' COMMENT '返回结果',\n" +
 			"PRIMARY KEY (`id`),\n" +
 			"KEY `ip` (`ip`),\n" +
 			"KEY `job_id` (`job_id`),\n" +
