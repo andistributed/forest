d18a1d6074d1b2ce1d3189391665ba50d780756c
Hank Shen <swhbox@foxmail.com>
2021-08-06 11:22:29 +0800

    update

--- api.go
+++ api.go
@@ -681,6 +681,7 @@ func (api *JobAPI) executeSnapshotRetry(context echo.Context) (err error) {
 	}
 	switch execSnapshot.Status {
 	case JobExecuteSnapshotErrorStatus:
+		// OK
 	case JobExecuteSnapshotUnknownStatus, JobExecuteSnapshotDoingStatus:
 		if api.canRetry(execSnapshot, time.Now().Local()) {
 			break
